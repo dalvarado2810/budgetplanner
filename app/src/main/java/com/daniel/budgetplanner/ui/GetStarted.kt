55de484ccb5b7650ad582e06bfbea274f84cc427
dalvarado2810 <enviromenttech@gmail.com>
2024-01-21 18:26:12 -0300

    New update of  code * add scroll in start page

--- app/src/main/java/com/daniel/budgetplanner/ui/GetStarted.kt
+++ app/src/main/java/com/daniel/budgetplanner/ui/GetStarted.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
@@ -63,6 +65,7 @@ fun GetStarted (
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(color = BackGround)
     ) {
         TopShape() {
